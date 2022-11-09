61d383b5c38b3310c6d9df40c33adf3ca600cad9
rifqi-anshari <46566132+rifqi-anshari@users.noreply.github.com>
2022-11-09 22:18:11 +0800

    Add files via upload

---
+++ config/tanggal.php
@@ -0,0 +1,120 @@
+<?php
+// function format_rupiah($angka){
+//   $rupiah=number_format($angka,0,',','.');
+//   return $rupiah;
+// }
+
+function Size($path)
+{
+	$bytes = sprintf('%u', filesize($path));
+
+	if ($bytes > 0) {
+		$unit = intval(log($bytes, 1024));
+		$units = array('B', 'KB', 'MB', 'GB');
+
+		if (array_key_exists($unit, $units) === true) {
+			return sprintf('%d %s', $bytes / pow(1024, $unit), $units[$unit]);
+		}
+	}
+
+	return $bytes;
+}
+
+function tgl_indo($tgl)
+{
+	$tanggal = substr($tgl, 8, 2);
+	$bulan = getBulan(substr($tgl, 5, 2));
+	$tahun = substr($tgl, 0, 4);
+	return $tanggal . ' ' . $bulan . ' ' . $tahun;
+}
+
+function tgl_indoo($tgl)
+{
+	$tanggal = substr($tgl, 8, 2);
+	$bulan = getBulann(substr($tgl, 5, 2));
+	$tahun = substr($tgl, 0, 4);
+	return $tanggal . ' ' . $bulan . ' ' . $tahun;
+}
+
+function getBulan($bln)
+{
+	switch ($bln) {
+		case 1:
+			return "Januari";
+			break;
+		case 2:
+			return "Februari";
+			break;
+		case 3:
+			return "Maret";
+			break;
+		case 4:
+			return "April";
+			break;
+		case 5:
+			return "Mei";
+			break;
+		case 6:
+			return "Juni";
+			break;
+		case 7:
+			return "Juli";
+			break;
+		case 8:
+			return "Agustus";
+			break;
+		case 9:
+			return "September";
+			break;
+		case 10:
+			return "Oktober";
+			break;
+		case 11:
+			return "November";
+			break;
+		case 12:
+			return "Desember";
+			break;
+	}
+}
+function getBulann($bln)
+{
+	switch ($bln) {
+		case 1:
+			return "Jan";
+			break;
+		case 2:
+			return "Feb";
+			break;
+		case 3:
+			return "Mar";
+			break;
+		case 4:
+			return "Apr";
+			break;
+		case 5:
+			return "Mei";
+			break;
+		case 6:
+			return "Jun";
+			break;
+		case 7:
+			return "Jul";
+			break;
+		case 8:
+			return "Agu";
+			break;
+		case 9:
+			return "Sep";
+			break;
+		case 10:
+			return "Okt";
+			break;
+		case 11:
+			return "Nov";
+			break;
+		case 12:
+			return "Des";
+			break;
+	}
+}
